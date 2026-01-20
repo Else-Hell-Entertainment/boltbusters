5969157222228db4e07de152a5354a9668656563
Pekka Heljakka <pekka.heljakka@tuni.fi>
2026-01-20 15:07:39 +0200

    Feat: Add command pattern based player movement
    - Add interface ICommand
    - Add CB3DMover script
    - Add input handler script
    - Add folders to project

--- Code/Objects/Player/PlayerController.cs
+++ Code/Objects/Player/PlayerController.cs
@@ -1,50 +1,7 @@
-using System.Collections.Generic;
+using System;
 using Godot;
 
 namespace EHE.BoltBusters
 {
-    public partial class PlayerController : CharacterBody3D
-    {
-        private CB3DMover _bodyMover;
-        private InputHandler _inputHandler;
-
-        private MoveToDirectionCommand _moveCommand = null;
-
-        public override void _Ready()
-        {
-            _bodyMover = new CB3DMover(this);
-            _inputHandler = new InputHandler();
-        }
-
-        public override void _Process(double delta)
-        {
-            HandleInput();
-        }
-
-        private void HandleInput()
-        {
-            List<ICommand> commandList = _inputHandler.GetInputCommands();
-            foreach (ICommand command in commandList)
-            {
-                if (command is MoveToDirectionCommand && _moveCommand == null)
-                {
-                    _moveCommand = command as MoveToDirectionCommand;
-                }
-            }
-        }
-
-        public override void _PhysicsProcess(double delta)
-        {
-            ExecuteCommands();
-        }
-
-        private void ExecuteCommands()
-        {
-            if (_moveCommand != null)
-            {
-                _moveCommand.Execute(_bodyMover);
-                _moveCommand = null;
-            }
-        }
-    }
+    public partial class PlayerController : CharacterBody3D { }
 }
