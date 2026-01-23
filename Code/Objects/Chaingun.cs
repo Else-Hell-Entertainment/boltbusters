0302110e2b3bfde52692bad2aef2ea0e7de4f8bd
Pekka Heljakka <pekka.heljakka@tuni.fi>
2026-01-23 12:37:48 +0200

    Feat: Add chaingun scene
    Stub scene for player controlled chaingun

--- Code/Objects/Chaingun.cs
+++ Code/Objects/Chaingun.cs
@@ -4,10 +4,29 @@ namespace EHE.BoltBusters
 {
     public partial class Chaingun : Node3D, IAttacker
     {
+        [Export] private Timer _cooldownTimer;
+        [Export] private float _cooldown = 0.5f;
+
+        private bool _canFire = true;
+
+        public override void _Ready()
+        {
+            _cooldownTimer.WaitTime = _cooldown;
+            _cooldownTimer.Timeout += OnCooldownTimerTimeout;
+        }
+
+        private void OnCooldownTimerTimeout()
+        {
+            GD.Print("Chaingun ready to fire.");
+            _canFire = true;
+        }
+
         public void Attack()
         {
             GD.Print("ChainGun goes PewPewPew");
             GD.Print("More pew.");
+            _canFire = false;
+            _cooldownTimer.Start();
         }
     }
 }
