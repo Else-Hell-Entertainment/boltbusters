28b428c417cae186ddd8a31dc93cef89b36dc67e
Miska Rihu <miska.rihu@tuni.fi>
2026-01-26 13:45:26 +0200

    Refactor: Move logic from IDamageable to HealthComponent
    Create new abstract base class for health component for handling health related functionality.
    
    Properties:
    - MaxHealth
    - CurrentHealth
    - IsAlive
    - IsImmortal
    
    Methods base logic:
    - Heal --> Increase
    - TakeDamage --> Decrease
    
    Handling death is not considered in the HealtComponent. When the entity dies, TakeDamage returns false.

---
+++ Code/Health/HealthComponent.cs
@@ -0,0 +1,103 @@
+using System;
+using Godot;
+
+namespace EHE.BoltBusters
+{
+    public abstract partial class HealthComponent : Node
+    {
+        [Export]
+        private int _maxHealth = 100;
+
+        private int _currentHealth;
+        private bool _isImmortal = false;
+
+        /// <summary>
+        /// The maximum health the entity can have.
+        /// </summary>
+        public int MaxHealth => _maxHealth;
+
+        /// <summary>
+        /// The current health of the entity.
+        /// Clamped between 0 and <see cref="MaxHealth"/>.
+        /// </summary>
+        public int CurrentHealth
+        {
+            get => _currentHealth;
+            protected set
+            {
+                _currentHealth = Math.Clamp(value, min: 0, max: _maxHealth);
+            }
+        }
+
+        /// <summary>
+        /// Is <see cref="CurrentHealth"/> greater than 0.
+        /// </summary>
+        public bool IsAlive => CurrentHealth > 0;
+
+        /// <summary>
+        /// If the entity can take damage or not.
+        /// </summary>
+        public bool IsImmortal
+        {
+            get => _isImmortal;
+            protected set => _isImmortal = value;
+        }
+
+        /// <summary>
+        /// Increases the health by the given amount.
+        /// </summary>
+        ///
+        /// <param name="amount">
+        /// The amount to increase health by.
+        /// </param>
+        ///
+        /// <remarks>
+        /// The amount must be a positive integer. If a negative value is
+        /// provided, it is automatically converted to a positive value.
+        /// </remarks>
+        public virtual void Increase(int amount)
+        {
+            if (amount < 0)
+            {
+                GD.PrintErr("Cannot increase health by negative amount, converting to positive.");
+                amount *= -1;
+            }
+
+            CurrentHealth = Math.Clamp(CurrentHealth + amount, min: 0, max: MaxHealth);
+        }
+
+        /// <summary>
+        /// Decreases the health by the given amount if <see cref="IsImmortal"/>
+        /// is <b>not</b> set to <c>true</c>.
+        /// </summary>
+        ///
+        /// <param name="amount">
+        /// The amount to decrease health by.
+        /// </param>
+        ///
+        /// <returns>
+        /// <c>true</c> if the entity is still alive after taking damage,
+        /// <c>false</c> otherwise.
+        /// </returns>
+        ///
+        /// <remarks>
+        /// The amount must be a positive integer. If a negative value is
+        /// provided, it is automatically converted to a positive value.
+        /// </remarks>
+        public virtual bool Decrease(int amount)
+        {
+            if (!IsImmortal)
+            {
+                if (amount < 0)
+                {
+                    GD.PrintErr("Cannot decrease health by negative amount, converting to positive.");
+                    amount *= -1;
+                }
+
+                CurrentHealth -= amount;
+            }
+
+            return IsAlive;
+        }
+    }
+}
