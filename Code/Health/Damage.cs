943e85c4fed9b306df45140b6a08a32693b5dfa9
Miska Rihu <miska.rihu@tuni.fi>
2026-01-26 14:10:18 +0200

    Feat: Add damage type and amount to damage class

--- Code/Health/Damage.cs
+++ Code/Health/Damage.cs
@@ -1,7 +1,28 @@
 namespace EHE.BoltBusters
 {
+    public enum DamageType
+    {
+        None = 0,
+        Chaingun,
+        Sniper,
+        Missile,
+    }
+
     public class Damage
     {
-        public Damage() { }
+        public int Amount { get; private set; }
+        public DamageType Type { get; private set; }
+
+        public Damage()
+        {
+            Amount = 0;
+            Type = DamageType.None;
+        }
+
+        public Damage(int amount, DamageType damageType)
+        {
+            Amount = amount;
+            Type = damageType;
+        }
     }
 }
