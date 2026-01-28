4efd3e09dd0c004b135592e3ac4253a6b2ca84cb
Pekka Heljakka <pekka.heljakka@tuni.fi>
2026-01-28 14:40:30 +0200

    Feat: Add rocket launcher scene

--- Code/Objects/RocketLauncher.cs
+++ Code/Objects/RocketLauncher.cs
@@ -2,10 +2,24 @@
 
 namespace EHE.BoltBusters
 {
-    public class RocketLauncher : IAttacker
+    public partial class RocketLauncher : BaseWeapon
     {
-        public void Attack()
+        [Export]
+        private PackedScene _rocketScene;
+
+        private Node3D _launchPoint;
+
+        public override void _Ready()
         {
+            _launchPoint = GetNode<Node3D>("LaunchPoint");
+        }
+
+        public override void Attack()
+        {
+            Rocket rocket = _rocketScene.Instantiate<Rocket>();
+            rocket.Position = _launchPoint.GlobalPosition;
+            rocket.Rotation = _launchPoint.GlobalRotation;
+            AddChild(rocket);
             GD.Print("Rocket launcher goes SKÄBÄDÄBÄDÄÄ");
         }
     }
