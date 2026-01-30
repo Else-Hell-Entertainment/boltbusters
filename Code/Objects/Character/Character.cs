e5092f198c52daa1070703cd529c33886d1f0dfe
Miska Rihu <miska.rihu@tuni.fi>
2026-01-30 12:36:55 +0200

    Feat: Base implementation for characters taking damage and dying

--- Code/Objects/Character/Character.cs
+++ Code/Objects/Character/Character.cs
@@ -12,9 +12,33 @@ namespace EHE.BoltBusters
             _healthComponent.Increase(amount);
         }
 
+        /// <summary>
+        /// Handles taking damage. By default, this accounts only for the
+        /// amount of damage defined by <paramref name="damageData"/> and
+        /// applies it to the character's health component. Additionally, if the
+        /// character's is considered dead after applying damage, executes the
+        /// <see cref="HandleDeath"/> method.
+        /// </summary>
+        /// <param name="damageData">Information about the damage that was dealt.</param>
         public virtual void TakeDamage(DamageData damageData)
         {
             _healthComponent.Decrease(damageData.Amount);
+
+            if (!_healthComponent.IsAlive)
+            {
+                HandleDeath();
+            }
+        }
+
+        /// <summary>
+        /// <b>WIP!</b>
+        /// Handles the death of a character. By default, this simply deletes
+        /// the character node from the scene using its <see cref="Node.QueueFree"/>
+        /// method.
+        /// </summary>
+        public void HandleDeath()
+        {
+            QueueFree();
         }
     }
 }
