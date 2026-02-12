ff9b88bcc311475c0000953587aca013fdd39ab6
Miska Rihu <miska.rihu@tuni.fi>
2026-02-12 19:03:19 +0200

    Feat: Implement loader scene
    Add script to the loader scene that instructs the the game manager to load the selected state on startup.

--- Code/Loader.cs
+++ Code/Loader.cs
@@ -1,7 +1,24 @@
-using System;
+// (c) 2026 Else Hell Entertainment
+// License: MIT License (see LICENSE in project root for details)
+// Author(s): Miska Rihu <miska.rihu@tuni.fi>
+//
+// Based on Sami Kojo's implementation.
+// Source:
+// License: https://github.com/samikojo-tuni/GArkanoid-2025/blob/c53ac400cc9fbd8855bc59f907ab751aba83c205/LICENSE
+
+using EHE.BoltBusters.States;
 using Godot;
 
 namespace EHE.BoltBusters
 {
-    public partial class Loader : Control { }
+    public partial class Loader : Control
+    {
+        [Export]
+        private StateType _initialState = StateType.MenuMain;
+
+        public override void _Ready()
+        {
+            GameManager.Instance.StateMachine.TransitionTo(_initialState);
+        }
+    }
 }
