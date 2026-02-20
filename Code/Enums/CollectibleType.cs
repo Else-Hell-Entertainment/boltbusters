c01eab38c5b75f51bce5207205fc743836f66610
TimeForNano <tuominen.mika-95@hotmail.com>
2026-02-20 16:34:17 +0200

    Feat: ICollectible and CollectibleSpawnManager with initial implementation

---
+++ Code/Enums/CollectibleType.cs
@@ -0,0 +1,14 @@
+// (c) 2026 Else Hell Entertainment
+// License: MIT License (see LICENSE in project root for details)
+// Author(s): TimeForNano tuominen.mika-95@hotmail.com
+
+namespace EHE.BoltBusters
+{
+    public enum CollectibleType
+    {
+        None = 0,
+        Nut,
+        Bolt,
+        Wrench,
+    }
+}
