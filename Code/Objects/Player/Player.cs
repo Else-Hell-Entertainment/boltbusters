859e6674bf7fe4ba9116bfe8fbe32727d9a6d7f3
Miska Rihu <miska.rihu@tuni.fi>
2026-02-28 22:28:29 +0200

    Feat: Cache weapon controllers in Player and setup upgrade handler

--- Code/Objects/Player/Player.cs
+++ Code/Objects/Player/Player.cs
@@ -1,10 +1,16 @@
 using System;
+using EHE.Common.Godot.Extensions;
 using Godot;
 
 namespace EHE.BoltBusters
 {
     public partial class Player : Character
     {
+        private PlayerChaingunController _chaingunController;
+        private PlayerRailgunController _railgunController;
+        private PlayerRocketLauncherController _rocketLauncherController;
+        private PlayerUpgradeHandler _upgradeHandler;
+
         /// <summary>
         /// (Parent) _EnterTree runs first and is good for registering and subscribing to services.
         /// </summary>
@@ -16,6 +22,17 @@ namespace EHE.BoltBusters
             }
 
             TargetProvider.Instance.RegisterPlayer(this);
+        }
+
+        public override void _Ready()
+        {
+            _chaingunController = this.GetFirstChildOfType<PlayerChaingunController>(true);
+            _railgunController = this.GetFirstChildOfType<PlayerRailgunController>(true);
+            _rocketLauncherController = this.GetFirstChildOfType<PlayerRocketLauncherController>(true);
+            _upgradeHandler = new PlayerUpgradeHandler();
+            _upgradeHandler.RegisterWeaponController(_chaingunController);
+            _upgradeHandler.RegisterWeaponController(_railgunController);
+            _upgradeHandler.RegisterWeaponController(_rocketLauncherController);
         }
 
         /// <summary>
