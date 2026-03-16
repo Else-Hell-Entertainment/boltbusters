e0d0fbb8b275aa7d3314b1062e8ee5fffc51190e
Pekka Heljakka <pekka.heljakka@tuni.fi>
2026-03-16 09:23:34 +0200

    Doc: Add missing comments

--- Code/Interfaces/Commands/StopMovementCommand.cs
+++ Code/Interfaces/Commands/StopMovementCommand.cs
@@ -1,14 +1,39 @@
-﻿namespace EHE.BoltBusters
+﻿// (c) 2026 Else Hell Entertainment
+// License: MIT License (see LICENSE in project root for details)
+// Author(s): Pekka Heljakka <pekka.heljakka@tuni.fi>
+
+namespace EHE.BoltBusters
 {
+    /// <summary>
+    /// Command that stops movement on an <see cref="EntityMover"/> receiver.
+    /// </summary>
+    /// <remarks>
+    /// Call <see cref="AssignReceiver(object)"/> before <see cref="Execute()"/> so the command
+    /// has a valid target to operate on.
+    /// </remarks>
     public class StopMovementCommand : ICommand
     {
-        EntityMover _mover;
+        /// <summary>
+        /// Receiver that performs the actual movement stop operation.
+        /// </summary>
+        private EntityMover _mover;
 
+        /// <summary>
+        /// Executes the command by stopping the assigned receiver's movement.
+        /// </summary>
         public void Execute()
         {
             _mover.StopMovement();
         }
 
+        /// <summary>
+        /// Assigns the command receiver.
+        /// </summary>
+        /// <param name="target">Object expected to be an <see cref="EntityMover"/>.</param>
+        /// <returns>
+        /// <see langword="true"/> when <paramref name="target"/> is a valid receiver and assignment succeeds;
+        /// otherwise, <see langword="false"/>.
+        /// </returns>
         public bool AssignReceiver(object target)
         {
             if (target is not EntityMover mover)
