5fa306f64e09729ee0ecee894da637030e0f7652
Pekka Heljakka <pekka.heljakka@tuni.fi>
2026-03-17 10:14:59 +0200

    Feat: Add PlayerConfigurationChanged signal to GameManager

--- Code/Systems/GameManager.cs
+++ Code/Systems/GameManager.cs
@@ -52,6 +52,9 @@ namespace EHE.BoltBusters
         [Signal]
         public delegate bool RequestWeaponDowngradeEventHandler(int weaponType);
 
+        [Signal]
+        public delegate void PlayerConfigurationChangedEventHandler();
+
         #endregion Signals
 
 
