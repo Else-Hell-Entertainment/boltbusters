da120769498b7a845c7552dbbea512057f8de2be
Pekka Heljakka <85389792+heljakkp@users.noreply.github.com>
2026-04-01 09:46:53 +0300

    Refactor: deprecate old files

--- Code/Objects/Enemy/EnemyAI/Deprecated_EnemyGroupAI.cs
+++ Code/Objects/Enemy/EnemyAI/Deprecated_EnemyGroupAI.cs
@@ -4,32 +4,8 @@ using Godot;
 
 namespace EHE.BoltBusters
 {
-    public partial class EnemyGroupAI : Node3D
+    public partial class Deprecated_EnemyGroupAI : Node3D
     {
-        [Export]
-        private EnemyCannonBot _bot1;
-
-        [Export]
-        private EnemyCannonBot _bot2;
-
-        [Export]
-        private EnemyCannonBot _bot3;
-
-        [Export]
-        private EnemyCannonBot _bot4;
-
-        [Export]
-        private EnemyCannonBot _bot5;
-
-        [Export]
-        private EnemyCannonBot _bot6;
-
-        [Export]
-        private EnemyCannonBot _bot7;
-
-        [Export]
-        private EnemyCannonBot _bot8;
-
         private List<IEnemyGroup> _groupList = new();
 
         private bool _isSurrounding = true;
@@ -39,28 +15,13 @@ namespace EHE.BoltBusters
         public override void _Ready()
         {
             base._Ready();
-            EnemyGroupCannonBotSurroundPlayer _surroundGroup = new EnemyGroupCannonBotSurroundPlayer();
-            EnemyGroupCannonBotDiamond _diamondGroup1 = new EnemyGroupCannonBotDiamond();
-            EnemyGroupCannonBotDiamond _diamondGroup2 = new EnemyGroupCannonBotDiamond();
+            Deprecated_EnemyGroupCannonBotSurroundPlayer _surroundGroup =
+                new Deprecated_EnemyGroupCannonBotSurroundPlayer();
+            Deprecated_EnemyGroupCannonBotDiamond _diamondGroup1 = new Deprecated_EnemyGroupCannonBotDiamond();
+            Deprecated_EnemyGroupCannonBotDiamond _diamondGroup2 = new Deprecated_EnemyGroupCannonBotDiamond();
             AddChild(_diamondGroup1);
             AddChild(_diamondGroup2);
             AddChild(_surroundGroup);
-            _surroundGroup.RegisterBot(_bot1);
-            _surroundGroup.RegisterBot(_bot2);
-            _surroundGroup.RegisterBot(_bot3);
-            _surroundGroup.RegisterBot(_bot4);
-            _surroundGroup.RegisterBot(_bot5);
-            _surroundGroup.RegisterBot(_bot6);
-            _surroundGroup.RegisterBot(_bot7);
-            _surroundGroup.RegisterBot(_bot8);
-            _diamondGroup1.RegisterBot(_bot1);
-            _diamondGroup1.RegisterBot(_bot2);
-            _diamondGroup1.RegisterBot(_bot3);
-            _diamondGroup1.RegisterBot(_bot4);
-            _diamondGroup2.RegisterBot(_bot5);
-            _diamondGroup2.RegisterBot(_bot6);
-            _diamondGroup2.RegisterBot(_bot7);
-            _diamondGroup2.RegisterBot(_bot8);
 
             _groupList.Add(_surroundGroup);
             _groupList.Add(_diamondGroup1);
@@ -80,7 +41,7 @@ namespace EHE.BoltBusters
                     _isSurrounding = false;
                     foreach (IEnemyGroup group in _groupList)
                     {
-                        if (group is EnemyGroupCannonBotSurroundPlayer surrounder)
+                        if (group is Deprecated_EnemyGroupCannonBotSurroundPlayer surrounder)
                         {
                             surrounder.IsActive = false;
                         }
@@ -95,7 +56,7 @@ namespace EHE.BoltBusters
                     _isSurrounding = true;
                     foreach (IEnemyGroup group in _groupList)
                     {
-                        if (group is EnemyGroupCannonBotSurroundPlayer surrounder)
+                        if (group is Deprecated_EnemyGroupCannonBotSurroundPlayer surrounder)
                         {
                             surrounder.IsActive = true;
                         }
---
+++ Code/Objects/Enemy/EnemyAI/GroupBehaviourCannonbotStandoff.cs
@@ -0,0 +1,7 @@
+﻿namespace BoltBusters.Code.Objects.Enemy.EnemyAI
+{
+    public class GroupBehaviourCannonbotStandoff
+    {
+        
+    }
+}
