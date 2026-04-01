815e6c31bfb3090e52babc203bcb53f8ad0d5321
Pekka Heljakka <85389792+heljakkp@users.noreply.github.com>
2026-04-01 09:57:20 +0300

    Feat: Add cannonbot standoff group

--- Code/Objects/Enemy/EnemyAI/GroupBehaviourCannonbotStandoff.cs
+++ Code/Objects/Enemy/EnemyAI/GroupBehaviourCannonbotStandoff.cs
@@ -1,7 +1,29 @@
-﻿namespace BoltBusters.Code.Objects.Enemy.EnemyAI
+﻿using EHE.BoltBusters;
+using EHE.BoltBusters.EnemyAI;
+using Godot;
+
+namespace EHE.BoltBusters.EnemyAI
 {
-    public class GroupBehaviourCannonbotStandoff
+    public partial class GroupBehaviourCannonbotStandoff : BaseGroupBehaviour
     {
-        
+        public override EnemyType AcceptedEnemyType => EnemyType.Ranged;
+
+        // Basically infinite.
+        public override int GroupSize => 500;
+
+        private float _distanceToPlayer = 6;
+
+        protected override void ExecuteGroupBehaviour()
+        {
+            foreach (Enemy enemy in Enemies)
+            {
+                if (enemy is EnemyCannonBot bot)
+                {
+                    Vector3 direction = (GlobalPosition - LevelManager.Active.Player.GlobalPosition).Normalized();
+                    Vector3 target = LevelManager.Active.Player.GlobalPosition + direction * _distanceToPlayer;
+                    bot.Controller.AddCommand(new MoveToPositionCommand(target));
+                }
+            }
+        }
     }
 }
