ee6bbc893c18f505d05e03219e9cbaa2c8eeee54
Pekka Heljakka <85389792+heljakkp@users.noreply.github.com>
2026-04-01 14:00:59 +0300

    Refactor: Change properties from public->protected

--- Code/Objects/Enemy/EnemyAI/BaseGroupBehaviour.cs
+++ Code/Objects/Enemy/EnemyAI/BaseGroupBehaviour.cs
@@ -1,18 +1,42 @@
-﻿using System.Collections.Generic;
+﻿// (c) 2026 Else Hell Entertainment
+// License: MIT License (see LICENSE in project root for details)
+// Author(s): Pekka Heljakka <pekka.heljakka@tuni.fi>
+
+using System.Collections.Generic;
 using Godot;
 
 namespace EHE.BoltBusters.EnemyAI
 {
+    /// <summary>
+    /// Base class for enemy group logic and membership management.
+    /// </summary>
     public abstract partial class BaseGroupBehaviour : Node3D
     {
-        public List<Enemy> Enemies { get; private set; } = new List<Enemy>();
+        /// <summary>
+        /// Current enemies registered to this group.
+        /// </summary>
+        protected List<Enemy> Enemies { get; private set; } = new List<Enemy>();
 
-        public abstract EnemyType AcceptedEnemyType { get; }
+        /// <summary>
+        /// Enemy type accepted by this group.
+        /// </summary>
+        protected abstract EnemyType AcceptedEnemyType { get; }
 
-        public abstract int GroupSize { get; }
+        /// <summary>
+        /// Maximum number of enemies in this group.
+        /// </summary>
+        protected abstract int GroupSize { get; }
 
+        /// <summary>
+        /// Enables or disables execution for this group.
+        /// </summary>
         public bool IsActive;
 
+        /// <summary>
+        /// Tries to register an enemy to this group.
+        /// </summary>
+        /// <param name="bot">Enemy to register.</param>
+        /// <returns>True if registration succeeded.</returns>
         public bool RegisterBot(Enemy bot)
         {
             if (bot.EnemyType != AcceptedEnemyType || Enemies.Count >= GroupSize || Enemies.Contains(bot))
@@ -29,6 +53,11 @@ namespace EHE.BoltBusters.EnemyAI
             return true;
         }
 
+        /// <summary>
+        /// Removes an enemy from this group.
+        /// </summary>
+        /// <param name="bot">Enemy to remove.</param>
+        /// <returns>True if the enemy was removed.</returns>
         public bool UnRegisterBot(Enemy bot)
         {
             if (Enemies.Contains(bot))
@@ -39,11 +68,17 @@ namespace EHE.BoltBusters.EnemyAI
             return false;
         }
 
+        /// <summary>
+        /// Removes all enemies from this group.
+        /// </summary>
         public void ClearBots()
         {
             Enemies.Clear();
         }
 
+        /// <summary>
+        /// Validates members and executes the group behaviour when active.
+        /// </summary>
         public void Execute()
         {
             if (!IsActive)
@@ -56,7 +91,7 @@ namespace EHE.BoltBusters.EnemyAI
 
         private void ValidateGroup()
         {
-            for (int i = 0; i < Enemies.Count; i++)
+            for (int i = Enemies.Count - 1; i >= 0; i--)
             {
                 if (!IsInstanceValid(Enemies[i]))
                 {
@@ -68,6 +103,9 @@ namespace EHE.BoltBusters.EnemyAI
             }
         }
 
+        /// <summary>
+        /// Executes behaviour logic for the current group members.
+        /// </summary>
         protected abstract void ExecuteGroupBehaviour();
     }
 }
