83373bb383bae7cba7df47bdbad599f4cc440c47
Pekka Heljakka <85389792+heljakkp@users.noreply.github.com>
2026-04-13 14:44:30 +0300

    Feat: Add working bossenemy melee weapon

--- Code/Objects/Enemy/BossMeleeWeapon.cs
+++ Code/Objects/Enemy/BossMeleeWeapon.cs
@@ -1,4 +1,7 @@
-using System;
+// (c) 2026 Else Hell Entertainment
+// License: MIT License (see LICENSE in project root for details)
+// Author(s): Pekka Heljakka <pekka.heljakka@tuni.fi>
+
 using Godot;
 
 namespace EHE.BoltBusters
@@ -7,7 +10,7 @@ namespace EHE.BoltBusters
     {
         [ExportGroup("Weapon Stats")]
         [Export]
-        private float _meleeCooldown;
+        private float _meleeCooldown = 1.0f;
 
         [Export]
         private int _meleeDamage;
@@ -25,9 +28,14 @@ namespace EHE.BoltBusters
         [Export]
         private AnimationPlayer _animationPlayer;
 
+        private const int COLLISION_MASK_PLAYER = 1;
+        private const string ATTACK_ANIMATION_NAME = "Attack";
+
         private DamageData _damageData;
 
         private bool _isAttacking;
+        private bool _attackOnCooldown;
+        private CharacterBody3D _player;
 
         public override void _Ready()
         {
@@ -35,11 +43,53 @@ namespace EHE.BoltBusters
             _cooldownTimer.WaitTime = _meleeCooldown;
             _cooldownTimer.Timeout += OnCooldownTimerTimeout;
             _damageData = new DamageData(_meleeDamage, DamageType.Melee);
+            _triggerArea.BodyEntered += OnTriggerAreaEntered;
+            _triggerArea.CollisionMask = COLLISION_MASK_PLAYER;
+            _attackArea.BodyEntered += OnAttackAreaEntered;
+            _player = LevelManager.Active.Player;
+        }
+
+        public void SetAttackState(bool isAttacking)
+        {
+            _isAttacking = isAttacking;
+            _attackArea.SetCollisionMaskValue(COLLISION_MASK_PLAYER, isAttacking);
+        }
+
+        private void TriggerAttack()
+        {
+            _animationPlayer.Play(ATTACK_ANIMATION_NAME);
+            _cooldownTimer.Start();
+            _attackOnCooldown = true;
+        }
+
+        private void OnTriggerAreaEntered(Node3D body)
+        {
+            if (body is Player)
+            {
+                TriggerAttack();
+            }
+        }
+
+        private void OnAttackAreaEntered(Node3D body)
+        {
+            if (body is Player player)
+            {
+                player.TakeDamage(_damageData);
+            }
         }
 
         private void OnCooldownTimerTimeout()
         {
-            throw new NotImplementedException();
+            _attackOnCooldown = false;
+            CheckTriggerArea();
+        }
+
+        private void CheckTriggerArea()
+        {
+            if (_triggerArea.OverlapsBody(_player))
+            {
+                TriggerAttack();
+            }
         }
     }
 }
