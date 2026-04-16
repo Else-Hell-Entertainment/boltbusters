1b943c2cdabd666aa7cace1a83e7c3e5a6ee3e1b
Pekka Heljakka <85389792+heljakkp@users.noreply.github.com>
2026-04-16 13:40:56 +0300

    Feat: Add railgun UI functionality Railgun UI now in final version

--- Code/UI/WeaponHud/RailgunHud.cs
+++ Code/UI/WeaponHud/RailgunHud.cs
@@ -14,25 +14,25 @@ namespace EHE.BoltBusters.Ui
 
         [ExportGroup("Node references")]
         [Export]
-        private TextureRect RailgunIcon1;
+        private TextureRect _railgunIcon1;
 
         [Export]
-        private TextureRect RailgunIcon2;
+        private TextureRect _railgunIcon2;
 
         [Export]
-        private TextureRect RailgunIcon3;
+        private TextureRect _railgunIcon3;
 
         [Export]
-        private TextureRect RailgunIcon4;
+        private TextureRect _railgunIcon4;
 
         public override void _Ready()
         {
             base._Ready();
             // With only 4 icons this is hardcoded for now (won't change).
-            _railgunIcons[0] = RailgunIcon1;
-            _railgunIcons[1] = RailgunIcon2;
-            _railgunIcons[2] = RailgunIcon3;
-            _railgunIcons[3] = RailgunIcon4;
+            _railgunIcons[0] = _railgunIcon1;
+            _railgunIcons[1] = _railgunIcon2;
+            _railgunIcons[2] = _railgunIcon3;
+            _railgunIcons[3] = _railgunIcon4;
 
             CallDeferred(MethodName.Initialize);
         }
@@ -42,11 +42,31 @@ namespace EHE.BoltBusters.Ui
             if (_railgunController != null)
             {
                 ClearRailgunList();
-                _railgunController.RailgunConfigurationChanged -= RefreshIcons;
+                _railgunController.RailgunConfigurationChanged -= OnConfigurationChanged;
             }
+
             _railgunController = LevelManager.Active.Player.RailgunController;
-            _railgunController.RailgunConfigurationChanged += RefreshIcons;
+            _railgunController.RailgunConfigurationChanged += OnConfigurationChanged;
             PopulateRailgunList();
+            RefreshIcons();
+        }
+
+        public override void _ExitTree()
+        {
+            base._ExitTree();
+
+            if (_railgunController != null)
+            {
+                ClearRailgunList();
+                _railgunController.RailgunConfigurationChanged -= OnConfigurationChanged;
+            }
+        }
+
+        private void OnConfigurationChanged()
+        {
+            ClearRailgunList();
+            PopulateRailgunList();
+            RefreshIcons();
         }
 
         private void PopulateRailgunList()
@@ -73,7 +93,10 @@ namespace EHE.BoltBusters.Ui
             }
         }
 
-        private void OnRailgunStateChanged(int state) { }
+        private void OnRailgunStateChanged(int state)
+        {
+            RefreshIcons();
+        }
 
         private void RefreshIcons()
         {
@@ -91,10 +114,41 @@ namespace EHE.BoltBusters.Ui
                 }
             }
 
+            int chargingCount = 0;
+            int readyToFireCount = 0;
+
             for (int i = 0; i < _railgunController.Weapons.Count; i++)
             {
-                //_railgunIcons[i].Show();
+                Railgun rg = (Railgun)_railgunController.Weapons[i];
+                if (rg.CurrentState == Railgun.RailgunState.Charging)
+                {
+                    chargingCount++;
+                }
+                else if (rg.CurrentState == Railgun.RailgunState.ReadyToFire)
+                {
+                    readyToFireCount++;
+                }
             }
+
+            for (int i = 0; i < readyToFireCount; i++)
+            {
+                SetIconStateReadyToFire(i);
+            }
+
+            for (int i = readyToFireCount; i < readyToFireCount + chargingCount; i++)
+            {
+                SetIconStateCharging(i);
+            }
+        }
+
+        private void SetIconStateCharging(int iconIndex)
+        {
+            _railgunIcons[iconIndex].Modulate = new Color(1, 1, 1, 0.2f);
+        }
+
+        private void SetIconStateReadyToFire(int iconIndex)
+        {
+            _railgunIcons[iconIndex].Modulate = new Color(1, 1, 1, 1f);
         }
     }
 }
