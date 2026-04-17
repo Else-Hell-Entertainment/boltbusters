b7b61b41ce6f22077e1e2d386ce56b475d85c9fc
Miska Rihu <229709985+misrihtuni@users.noreply.github.com>
2026-04-17 18:59:11 +0300

    Docs: Fix inconsistent contributor naming style in copyright notices

--- Code/UI/MenuVictory.cs
+++ Code/UI/MenuVictory.cs
@@ -1,6 +1,6 @@
 // (c) 2026 Else Hell Entertainment
 // License: MIT License (see LICENSE in project root for details)
-// Author(s): Rihu, Miska <miska.rihu@tuni.fi>
+// Author(s): Miska Rihu <miska.rihu@tuni.fi>
 
 using System;
 using EHE.BoltBusters.States;
