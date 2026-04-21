73c924bd97de2bf10b11be5c6c6dc148a8b826fa
Miska Rihu <229709985+misrihtuni@users.noreply.github.com>
2026-04-21 12:01:42 +0300

    WIP/Feat: Modify IUpgradeable to be scalable with UpgradeType
    - Supercede `PriceInfo` property with `GetPrice(UpradeType)` and `SetPrice(UpgradeType, PriceInfo)` methods.
    - Supercede `Upgrade()` method with `Upgrade(UpgradeType)` method.
    - Supercede `Downgrade()` method with `Downgrade(UpgradeType)` method.

--- Code/Interfaces/IUpgradeable.cs
+++ Code/Interfaces/IUpgradeable.cs
@@ -2,6 +2,8 @@
 // License: MIT License (see LICENSE in project root for details)
 // Author(s): Miska Rihu <miska.rihu@tuni.fi>
 
+using System;
+
 namespace EHE.BoltBusters
 {
     /// <summary>
@@ -12,26 +14,81 @@ namespace EHE.BoltBusters
         /// <summary>
         ///  Price of the upgrade.
         /// </summary>
-        public PriceInfo PriceInfo { get; }
+        [Obsolete("Use GetPrice and SetPrice with an internal data structure.")]
+        PriceInfo PriceInfo { get; }
 
         /// <summary>
-        ///  Use this method to perform upgrades.
+        ///  Returns the price for the given upgrade.
+        /// </summary>
+        ///
+        /// <param name="upgradeType">
+        ///  The upgrade whose price is to be queried.
+        /// </param>
+        ///
+        /// <returns>
+        ///  A <see cref="PriceInfo"/> object representing the price of the
+        ///  given <see cref="UpgradeType"/>.
+        /// </returns>
+        PriceInfo GetPrice(UpgradeType upgradeType);
+
+        /// <summary>
+        ///  Sets the price for the given upgrade type.
+        /// </summary>
+        ///
+        /// <param name="upgradeType">
+        ///  The upgrade whose price is to be modified.
+        /// </param>
+        /// <param name="priceInfo">The new price for the upgrade.</param>
+        bool SetPrice(UpgradeType upgradeType, PriceInfo priceInfo);
+
+        /// <summary>
+        ///  DEPRECATED! Use this method to perform upgrades.
         /// </summary>
         ///
         /// <returns>
         ///  <c>true</c> if upgrade was successful,
         ///  <c>false</c> otherwise.
         /// </returns>
-        public bool Upgrade();
+        [Obsolete("Use Upgrade(UpgradeType).")]
+        bool Upgrade();
 
         /// <summary>
-        ///  Use this method to perform downgrades.
+        ///  Performs an upgrade of the given type.
+        /// </summary>
+        ///
+        /// <param name="type">
+        ///  The type of the upgrade to perform.
+        /// </param>
+        ///
+        /// <returns>
+        ///  <c>true</c> if upgrade was successful,
+        ///  <c>false</c> otherwise.
+        /// </returns>
+        bool Upgrade(UpgradeType type);
+
+        /// <summary>
+        ///  DEPRECATED! Use this method to perform downgrades.
         /// </summary>
         ///
         /// <returns>
         ///  <c>true</c> if downgrade was successful,
         ///  <c>false</c> otherwise.
         /// </returns>
-        public bool Downgrade();
+        [Obsolete("Use Downgrade(UpgradeType).")]
+        bool Downgrade();
+
+        /// <summary>
+        ///  Performs a downgrade of the given type.
+        /// </summary>
+        ///
+        /// <param name="type">
+        ///  The type of the downgrade to perform.
+        /// </param>
+        ///
+        /// <returns>
+        ///  <c>true</c> if upgrade was successful,
+        ///  <c>false</c> otherwise.
+        /// </returns>
+        bool Downgrade(UpgradeType type);
     }
 }
