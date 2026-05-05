eaad58575c4745a6705b243541a2ea747e038f0e
TimeForNano <tuominen.mika-95@hotmail.com>
2026-05-05 19:08:45 +0300

    Fix: Credits thing showing

--- Code/UI/MenuVictory.cs
+++ Code/UI/MenuVictory.cs
@@ -2,9 +2,7 @@
 // License: MIT License (see LICENSE in project root for details)
 // Author(s): Miska Rihu <miska.rihu@tuni.fi>
 
-using System;
 using EHE.BoltBusters.States;
-using EHE.BoltBusters.Ui;
 using EHE.Common.Godot.Logging;
 using Godot;
 
@@ -12,8 +10,8 @@ namespace EHE.BoltBusters.Ui
 {
     public partial class MenuVictory : Menu
     {
-        [Export]
-        private Button _btnCredits;
+        // [Export]
+        // private Button _btnCredits;
 
         [Export]
         private Button _btnMainMenu;
@@ -69,11 +67,11 @@ namespace EHE.BoltBusters.Ui
             _btnQuit.Pressed -= OnBtnQuitPressed;
         }
 
-        private void OnBtnCreditsPressed()
-        {
-            MusicManager.Instance.ButtonSoundPlayer.Play();
-            throw new NotImplementedException("No credits window implemented.");
-        }
+        // private void OnBtnCreditsPressed()
+        // {
+        //     MusicManager.Instance.ButtonSoundPlayer.Play();
+        //     throw new NotImplementedException("No credits window implemented.");
+        // }
 
         private void OnBtnMainMenuPressed()
         {
